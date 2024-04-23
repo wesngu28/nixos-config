7758302365a4083c6b322f0736709eeead37b25e
wesngu28 <wesley@wesngu28.com>
2024-04-23 08:31:04 -0700

    Regenerate hardware

--- hosts/enterprise/configuration.nix
+++ hosts/enterprise/configuration.nix
@@ -30,6 +30,8 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
+  security.polkit.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
