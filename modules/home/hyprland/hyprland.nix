40a71f24cc0bc9ed02405eb276c7f1a065cf0b89
wesngu28 <wesley@wesngu28.com>
2024-05-02 23:16:16 -0700

    Split up main hyprland conf

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -1,199 +1,16 @@
 {
   inputs,
   pkgs,
-  lib,
   ...
-}: let
-  wallpaper = "test.jpg";
-in {
+}: {
   imports = [
-    inputs.hyprpaper.homeManagerModules.hyprpaper
-    inputs.hyprlock.homeManagerModules.hyprlock
-    inputs.hypridle.homeManagerModules.hypridle
+    ./hyprpaper.nix
+    ./hypridle.nix
   ];
+
   home.packages = [
     inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
   ];
-
-  services.hyprpaper = {
-    enable = true;
-    preloads = [
-      "~/Wallpapers/${wallpaper}"
-    ];
-
-    wallpapers = [
-      ",~/Wallpapers/${wallpaper}"
-    ];
-  };
-
-  programs.hyprlock = {
-    enable = true;
-
-    backgrounds = [
-      {
-        monitor = "";
-        path = "~/Wallpapers/${wallpaper}";
-        blur_size = 4;
-        blur_passes = 3;
-        brightness = 0.75;
-      }
-    ];
-
-    input-fields = [
-      {
-        monitor = "";
-        size = {
-          width = 250;
-          height = 60;
-        };
-        outline_thickness = 2;
-        dots_size = 0.2;
-        dots_spacing = 0.2;
-        dots_center = true;
-        outer_color = "rgb(11111b)";
-        inner_color = "rgb(585b70)";
-        font_color = "rgb(cdd6f4)";
-        fade_on_empty = false;
-        placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
-        hide_input = false;
-        position = {
-          x = 0;
-          y = -120;
-        };
-        halign = "center";
-        valign = "center";
-      }
-    ];
-    labels = [
-      {
-        monitor = "";
-        text = "$TIME";
-        font_size = 120;
-        position = {
-          x = 0;
-          y = 80;
-        };
-        valign = "center";
-        halign = "center";
-      }
-    ];
-  };
-
-  services.hypridle = {
-    enable = true;
-    package = pkgs.hypridle;
-    lockCmd = "${pkgs.hyprlock}/bin/hyprlock";
-    beforeSleepCmd = "loginctl lock-session";
-    listeners = [
-      {
-        timeout = 300;
-        onTimeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10% & echo 1 | ${pkgs.coreutils}/bin/tee  /sys/class/leds/asus::kbd_backlight/brightness";
-        onResume = "${pkgs.brightnessctl}/bin/brightnessctl -r & echo 2 | ${pkgs.coreutils}/bin/tee /sys/class/leds/asus::kbd_backlight/brightness";
-      }
-      {
-        timeout = 350;
-        onTimeout = "hyperctl dispatch dpms off";
-        onResume = "hyperctl dispatch dpms on";
-      }
-      {
-        timeout = 1800;
-        onTimeout = "${pkgs.systemd}/bin/systemctl suspend";
-        onResume = "";
-      }
-    ];
-  };
-
-  programs.wlogout = {
-    enable = true;
-  };
-
-  xdg.configFile."wlogout/layout".text = ''
-    {
-        "label" : "lock",
-        "action" : "${inputs.hyprlock.packages.${pkgs.system}.hyprlock}/bin/hyprlock",
-        "text" : "Lock",
-        "keybind" : "l"
-    }
-    {
-        "label" : "hibernate",
-        "action" : "systemctl hibernate",
-        "text" : "Hibernate",
-        "keybind" : "h"
-    }
-    {
-        "label" : "logout",
-        "action" : "loginctl terminate-user $USER",
-        "text" : "Logout",
-        "keybind" : "e"
-    }
-    {
-        "label" : "shutdown",
-        "action" : "systemctl poweroff",
-        "text" : "Shutdown",
-        "keybind" : "s"
-    }
-    {
-        "label" : "suspend",
-        "action" : "systemctl suspend",
-        "text" : "Suspend",
-        "keybind" : "u"
-    }
-    {
-        "label" : "reboot",
-        "action" : "systemctl reboot",
-        "text" : "Reboot",
-        "keybind" : "r"
-    }
-  '';
-
-  xdg.configFile."wlogout/styles.css".text = ''
-      * {
-      background-image: none;
-    }
-
-    window {
-      background-color: rgba(12, 12, 12, 1);
-    }
-
-    button {
-      color: #ffffff;
-      background-color: #1e1e1e;
-      border-radius: 20px;
-      background-repeat: no-repeat;
-      background-position: center;
-      background-size: 50%;
-      margin: 10px;
-    }
-
-    button:hover {
-      background-color: #3b393d;
-      outline-style: none;
-    }
-
-    #lock {
-      background-image: image(url("/etc/wlogout-icons/lock.png"));
-    }
-
-    #logout {
-      background-image: image(url("/etc/wlogout-icons/logout.png"));
-    }
-
-    #suspend {
-      background-image: image(url("/etc/wlogout-icons/suspend.png"));
-    }
-
-    #hibernate {
-      background-image: image(url("/etc/wlogout-icons/hibernate.png"));
-    }
-
-    #shutdown {
-      background-image: image(url("/etc/wlogout-icons/shutdown.png"));
-    }
-
-    #reboot {
-      background-image: image(url("/etc/wlogout-icons/reboot.png"));
-    }
-  '';
 
   wayland.windowManager.hyprland = {
     enable = true;
@@ -221,6 +38,8 @@ in {
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user start graphical-session.target"
+        "thunar --daemon"
+        "wallpaper"
       ];
 
       general = {
@@ -327,7 +146,7 @@ in {
         "$mainMod SHIFT, 8, movetoworkspace, 8"
         "$mainMod SHIFT, 9, movetoworkspace, 9"
 
-        "$mainMod CTRL, P, exec, pkill waybar && hyprctl dispatch exec waybar"
+        "$mainMod SHIFT, b, exec, hyprctl dispatch exec waybar"
         "$mainMod, S, exec, grimblast copy area"
 
         # "$mainMod, mouse_down, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%+"
