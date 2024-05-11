c4aef768d923dbbee2baa332f3de5ead2bab04ba
wesngu28 <wesley@wesngu28.com>
2024-05-11 16:47:51 -0700

    kde connect

---
+++ modules/programs/kdeconnect.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}: {
+  programs.kdeconnect.enable = true;
+  environment.systemPackages = with pkgs; [
+    libsForQt5.kdeconnect-kde
+  ];
+  networking.firewall = {
+    enable = true;
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      } # KDE Connect
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      } # KDE Connect
+    ];
+  };
+}
