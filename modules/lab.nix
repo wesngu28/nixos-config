1aed97ef485b3700ee76066c1c7921fd13bd925c
wesngu28 <wesley@wesngu28.com>
2024-05-21 02:27:31 -0700

    Add other containers I use / on other debian servers, lab module

---
+++ modules/lab.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  imports = [
+    ./server/docker.nix
+    ./server/nginx.nix
+    ./server/containers/containers.nix
+    ./server/containers/media.nix
+    ./server/containers/rss.nix
+    # ./server/containers/syncthing.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    veracrypt
+  ];
+}
---
+++ modules/server/containers/containers.nix
@@ -0,0 +1,129 @@
+{
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      # "audiobookshelf" = {
+      #   autoStart = true;
+      #   image = "lscr.io/linuxserver/freshrss:latest";
+      #   ports = ["7755:80"];
+      #   volumes = [
+      #     "/home/serpe/docker/freshrss:/config"
+      #   ];
+      #   environment = {
+      #     PUID = "1000";
+      #     PGID = "1000";
+      #     TZ = "America/Phoenix";
+      #   };
+      #   extraOptions = [
+      #     "--network=immich"
+      #   ];
+      # };
+
+      # "calibre" = {
+      #   autoStart = true;
+      #   image = "lscr.io/linuxserver/freshrss:latest";
+      #   ports = ["7755:80"];
+      #   volumes = [
+      #     "/home/serpe/docker/freshrss:/config"
+      #   ];
+      #   environment = {
+      #     PUID = "1000";
+      #     PGID = "1000";
+      #     TZ = "America/Phoenix";
+      #   };
+      #   extraOptions = [
+      #     "--network=immich"
+      #   ];
+      # };
+
+      "jdownloader2" = {
+        autoStart = true;
+        image = "jlesage/jdownloader-2";
+        ports = ["5800:5800"];
+        volumes = [
+          "/home/serpe/docker/media/downloads:/output:rw"
+          "/home/serpe/docker/jdownloader:/config:rw"
+        ];
+      };
+
+      "glances" = {
+        autoStart = true;
+        image = "nicolargo/glances:latest-full";
+        ports = ["61208:61208"];
+        volumes = [
+          "/home/nixos-config/modules/lab/glances.conf:/glances.conf"
+          "/:/rootfs:ro"
+          "/var/run/docker.sock:/var/run/docker.sock"
+        ];
+        environment = {
+          GLANCES_OPT = "-w";
+        };
+      };
+
+      # "kuma" = {
+      #   autoStart = true;
+      #   image = "lscr.io/linuxserver/freshrss:latest";
+      #   ports = ["7755:80"];
+      #   volumes = [
+      #     "/home/serpe/docker/freshrss:/config"
+      #   ];
+      #   environment = {
+      #     PUID = "1000";
+      #     PGID = "1000";
+      #     TZ = "America/Phoenix";
+      #   };
+      #   extraOptions = [
+      #     "--network=immich"
+      #   ];
+      # };
+
+      # eventually, but already in other server
+      # "overleaf" = {
+      #   autoStart = true;
+      #   image = "lscr.io/linuxserver/freshrss:latest";
+      #   ports = ["7755:80"];
+      #   volumes = [
+      #     "/home/serpe/docker/freshrss:/config"
+      #   ];
+      #   environment = {
+      #     PUID = "1000";
+      #     PGID = "1000";
+      #     TZ = "America/Phoenix";
+      #   };
+      #   extraOptions = [
+      #     "--network=immich"
+      #   ];
+      # };
+
+      "navidrome" = {
+        autoStart = true;
+        image = "deluan/navidrome:latest";
+        ports = ["4533:4533"];
+        volumes = [
+          "/home/serpe/docker/navidrome:/config"
+          "/home/multimedia/music:/music"
+        ];
+      };
+
+      "picard" = {
+        autoStart = true;
+        image = "mikenye/picard:latest";
+        ports = ["5800:5800"];
+        volumes = [
+          "/home/serpe/docker/picard:/config"
+          "/home/multimedia/music:/storage:rw"
+        ];
+      };
+
+      "wallos" = {
+        autoStart = true;
+        image = "bellamy/wallos:latest";
+        ports = ["8282:80"];
+        volumes = [
+          "/home/serpe/docker/wallos/db:/var/www/html/db"
+          "/home/serpe/docker/wallos/logos:/var/www/html/images/uploads/logos"
+        ];
+      };
+    };
+  };
+}
