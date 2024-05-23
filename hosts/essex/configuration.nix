26f858145890799ea80ca23dc8d79e0d36778870
wesngu28 <wesley@wesngu28.com>
2024-05-23 00:48:57 -0700

    veracrypt

--- hosts/essex/configuration.nix
+++ hosts/essex/configuration.nix
@@ -10,64 +10,33 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.useDHCP = true;
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  fileSystems."/mnt/veracrypt1" = {
+    device = "/dev/disk/by-uuid/4E9EA45F9EA44177";
+    fsType = "ntfs";
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  environment.systemPackages = with pkgs; [
+    (pkgs.writeShellScriptBin "decrypt" ''
+      cd /mnt/veracrypt1
+      ${pkgs.veracrypt}/bin/veracrypt --mount Files
+      cd ~
+    '')
+  ];
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+  time.timeZone = "America/Phoenix";
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  users.users.serpe = {
+    initialPassword = "pw123";
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
 
-
-
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.serpe = {
-initialPassword = "pw123";
-   isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-   };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   programs.git = {
     enable = true;
     config = {
@@ -75,48 +44,11 @@ initialPassword = "pw123";
     user.email = "wesley@wesngu28.com";
     };
   };
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
--- modules/lab.nix
+++ modules/lab.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   imports = [
     ./server/docker.nix
-    ./server/nginx.nix
     ./server/containers/containers.nix
     ./server/containers/media.nix
     ./server/containers/rss.nix
