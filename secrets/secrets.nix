9caeb65547958f8e1b3de718d14e875afdd9eff5
wesngu28 <wesley@wesngu28.com>
2024-05-24 19:39:22 -0700

    age some server stuff

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,12 @@
 let
-  serpe_enterprise = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGnjKj4EyU0zid0YX/Ggj+UiqQYYeVB5pKSsP/rtY1tU serpe";
+  serpe = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGnjKj4EyU0zid0YX/Ggj+UiqQYYeVB5pKSsP/rtY1tU serpe";
 
-  enterprise = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAYyHNZ9+1Rm3WfqfHszDPZ2UHNCw6BduNGoKkES9OFK";
-  essex = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFj7MVayLlxwlyBozk1ncYm8vrqYYCNv/Qb2PWT269X";
-
-  users = [serpe_enterprise];
-  systems = [enterprise essex];
+  server_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP4PclanQT+dFpvM9xFbJSNxJpIHZzZHkXbxgIMvX6YX root@nixos";
+  users = [ serpe ];
 in {
-  "enterprise_wg0.age".publicKeys = [serpe_enterprise];
+  "enterprise_wg0.age".publicKeys = [ serpe ];
 
-  "feedcord.age".publicKeys = [serpe_enterprise];
+  "feedcord.age".publicKeys = [ serpe server_host];
 
-  "navidrome.age".publicKeys = [serpe_enterprise];
+  "navidrome.age".publicKeys = [ serpe server_host];
 }
