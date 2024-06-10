fbdb9da73fc18b9828e75bc09cca4126e15bb987
wesngu28 <wesley@wesngu28.com>
2024-06-10 01:46:31 -0700

    script to bring up and tear down bridge

--- hosts/enterprise/libvirtd.nix
+++ hosts/enterprise/libvirtd.nix
@@ -40,5 +40,19 @@
     #   win-virtio
     #   win-spice
     gnome.adwaita-icon-theme
+    (writeShellScriptBin
+      "bridge-up" ''
+        sudo nmcli con add ifname br0 type bridge con-name br0
+        sudo nmcli con add type bridge-slave ifname enp34s0 master br0
+        sudo nmcli con modify br0 bridge.stp no
+        sudo nmcli con down "Wired connection 1"
+        sudo nmcli con up br0
+      '')
+    (writeShellScriptBin
+      "bridge-down" ''
+        sudo nmcli con delete br0
+        sudo nmcli con delete bridge-slave-enp34s0
+        sudo nmcli con up "Wired connection 1"
+      '')
   ];
 }
