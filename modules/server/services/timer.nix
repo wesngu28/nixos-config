f4e87e589b11bb2883a2dbad750a102b5d80f33e
wesngu28 <wesley@wesngu28.com>
2024-12-22 10:32:14 -0700

    systemd timer

--- modules/server/services/timer.nix
+++ modules/server/services/timer.nix
@@ -1,8 +1,15 @@
 {
-  services.cron = {
-    enable = true;
-    systemCronJobs = [
-      "59 8,20 * * * /home/serpe/NotReproducible/check.sh"
-    ];
+  systemd.timers."check-timer" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig.OnCalendar = ["08:59 America/Los_Angeles" "20:59 America/Los_Angeles"];
+    timerConfig.Unit = "check-timer.service";
+  };
+
+  systemd.services."check-timer" = {
+    script = "/home/serpe/NotReproducible/check.sh";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "serpe";
+    };
   };
 }
